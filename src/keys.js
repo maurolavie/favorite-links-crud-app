2c1bcb774fcad71b0ca6fad94dc12c0dc1c78be1
Mauro Lavie <19332716+maurolavie@users.noreply.github.com>
2019-09-25 08:27:37 -0300

    Add files via upload

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    database: {
+        host: 'b5jnuzwtp0sexeb9jvbp-mysql.services.clever-cloud.com',
+        user: 'uvhszqyhlee4hgur',
+        password: 'lnnqQZG2wJGmjlOqtKDM',
+        database: 'b5jnuzwtp0sexeb9jvbp'
+    }
+
+};
